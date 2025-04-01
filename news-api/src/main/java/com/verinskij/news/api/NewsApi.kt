906ac4d36d7e2e2d5b4997b466c22515b3bb5f38
Vlad Ver <v.verinskij@mobilexapps.com>
2025-04-01 10:30:06 +0400

    cleaned import

--- news-api/src/main/java/com/verinskij/news/api/NewsApi.kt
+++ news-api/src/main/java/com/verinskij/news/api/NewsApi.kt
@@ -4,15 +4,12 @@ import com.skydoves.retrofit.adapters.result.ResultCallAdapterFactory
 import com.verinskij.news.api.model.ArticleDTO
 import com.verinskij.news.api.model.Language
 import com.verinskij.news.api.model.ResponseDTO
-import com.verinskij.news.api.model.SortBy
 import com.verinskij.news.api.utils.NewsApiKeyInterceptor
 import okhttp3.OkHttpClient
-
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
-import java.util.Date
 
 interface NewsApi {
     @GET("everything")
