48ddcf04e22d2e4c311e3fda01004fd8de52531c
Vlad Ver <v.verinskij@mobilexapps.com>
2025-04-01 10:24:12 +0400

    fix

--- news-api/src/main/java/com/verinskij/news/api/model/ArticleDTO.kt
+++ news-api/src/main/java/com/verinskij/news/api/model/ArticleDTO.kt
@@ -3,22 +3,6 @@ package com.verinskij.news.api.model
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
-/**
- * {
- * -"source": {
- * "id": null,
- * "name": "Ambcrypto.com"
- * },
- * "author": "Denis Mwirigi",
- * "title": "Cardano’s recovery strengthens – ADA eyes breakout above THIS level - AMBCrypto News",
- * "description": "ADA’s outlook suggests long-term strength as market participation and sentiment drive Cardano’s bullish momentum.",
- * "url": "https://ambcrypto.com/cardanos-recovery-strengthens-ada-eyes-breakout-above-this-level/",
- * "urlToImage": "https://ambcrypto.com/wp-content/uploads/2025/02/Editors-34-1000x600.webp",
- * "publishedAt": "2025-02-24T08:01:48Z",
- * "content": "<ul><li>Cardanos ascending triangle formation signals investor accumulation and higher chances of a bullish breakout.</li><li>Rising Open Interest, network activity and bullish market sentiment allud… [+2224 chars]"
- * },
- */
-
 @Serializable
 data class ArticleDTO(
     @SerialName("source") val source: SourceDTO,
